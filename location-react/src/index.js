fe1e4d56c41fe69dcf839ab8739f2944ad403925
Karthik Venkateswaran <karthik@hasura.io>
2018-09-04 15:56:40 +0530

    Removes router

--- location-react/src/index.js
+++ location-react/src/index.js
@@ -5,15 +5,8 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Driver from './Driver/Driver';
 
 // Use the client just as before
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const Main = () => (
-  <Router>
-    <div>
-      <Route exact path="/realtime_location_app" component={Driver} />
-    </div>
-  </Router>
-);
 ReactDOM.render(
-  <Main />,
+  <Driver />,
   document.getElementById('root'));
